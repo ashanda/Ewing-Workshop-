dbfea88950dfadbfa789c8e6a8270a70129c42ff
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-01-26 18:09:37 +0530

    Update footer.php
    update

--- wp-content/themes/ewing-workshop/footer.php
+++ wp-content/themes/ewing-workshop/footer.php
@@ -19,6 +19,8 @@
 		
 		<!--script-->		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/@fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/inc_files/scripts/lightslider/src/js/lightslider.js"></script>
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/inc_files/scripts/ui.js"></script>
 		<!-- masonry -->
@@ -32,8 +34,35 @@
 				  gutter: 10
 			});
 		</script>
+
+<!-- fancybox master -->
+
+		<script type="text/javascript">
+		  $("[data-fancybox]").fancybox({
+			// Options will go here
+          buttons : [
+            'close'
+          ],
+          wheel : false,
+          transitionEffect: "slide",
+           // thumbs          : false,
+          // hash            : false,
+          loop            : true,
+          // keyboard        : true,
+          toolbar         : false,
+          // animationEffect : false,
+          // arrows          : true,
+          clickContent    : false
+		  });
+		</script>
+		
+
+  
 		<?php wp_footer(); ?>
 	</body>
 </html>
+
+
+
 
 
