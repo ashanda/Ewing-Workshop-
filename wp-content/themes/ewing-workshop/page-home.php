2caf8489f2e721c0b3c085e36997be7bffc5d785
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-01-26 18:11:27 +0530

    Update page-home.php
    update

--- wp-content/themes/ewing-workshop/page-home.php
+++ wp-content/themes/ewing-workshop/page-home.php
@@ -6,9 +6,9 @@ Template Name: Home
 
 <div id="content-wrapper">					
 			<div id="home-content">
-			<div class="row"><div><p>Victors<br/> Artifacts</p></div></div>
-			<div class="row"><div><p>Film &<br/> Television</p></div></div>
-			<div class="row last-child"><div><p>Commissions</p></div></div>
+			<div class="row"><a href="<?php echo get_home_url(); ?>/work_category/victors-artfacts"><div><p>Victors<br/> Artifacts</p></div></a></div>
+			<div class="row"><a href="<?php echo get_home_url(); ?>/work_category/film-television"><div><p>Film &<br/> Television</p></div></a></div>
+			<div class="row last-child"><a href="<?php echo get_home_url(); ?>/work_category/commissions"><div><p>Commissions</p></div></a></div>
 		</div>
 </div><!--content wrapper end-->
 
